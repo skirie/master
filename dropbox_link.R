eb5b6b5fdebcc6702178e270006fe8a7fc1dd4ff
Ferdinand Briegel <ferdinand.briegel@meteo.local>
2018-10-16 15:52:30 +0200

    1610

--- dropbox_link.R
+++ dropbox_link.R
@@ -1,44 +1,46 @@
 #### ------------------------------------- ####
-## Dropbox Link für Daten
+  ## Dropbox Link für Daten
 #### ------------------------------------- ####
-
-## Package Function ####
-check.packages <- function(pkg){
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new.pkg)) 
-    install.packages(new.pkg, dependencies = TRUE)
-  sapply(pkg, require, character.only = TRUE)
-}
-
-## Usage ####
-packages <- c("httpuv", "rdrop2")
-check.packages(packages)
-
+  
+  ## Package Function ####
+  check.packages <- function(pkg){
+    new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+    if (length(new.pkg)) 
+      install.packages(new.pkg, dependencies = TRUE)
+    sapply(pkg, require, character.only = TRUE)
+  }
+  
+  ## Usage ####
+  packages <- c("httpuv", "rdrop2")
+  check.packages(packages)
+  
 #### ------------------------------------- ####
-## Daten einladen
+  ## Daten einladen
 #### ------------------------------------- ####
-
+  
 #### Daten einladen ####
-## path 
+  ## path 
+  
+  #setwd("E:/Uni/05_Masterarbeit/Auswertung") 
+  mypath <- getwd()
+  
+#### Workspace speichern
+  save.image(paste0(mypath, "/save.RData"))
 
-setwd("E:/Uni/05_Masterarbeit/Daten_und_Auswertung/Auswertung") 
-
-## Verbindung zu dropbox aufbauen ####
-#token <- drop_auth()
-#saveRDS(token, file = "token.rds")
-token <- readRDS("token.rds")
-
-## Workspace runterladen ####
-drop_download('Master/R/.RData', overwrite = T, dtoken = token)
-
-## Script runterladen
-drop_download('Master/R/Master_ANN.R', overwrite = T, dtoken = token)
-file.edit('Master_ANN.R')
-
-
-## upload WOrkspace ####
-drop_upload('.RData', 'Master/R/', mode = "overwrite", dtoken = token)
-
-drop_upload('Master_ANN.R', 'Master/R/', mode = "overwrite", dtoken = token)
-
-
+#### Verbindung zu dropbox aufbauen ####
+  #token <- drop_auth()
+  #saveRDS(token, file = "token.rds")
+  token <- readRDS("token.rds")
+  
+  ## Workspace runterladen ####
+  drop_download('Master/R/.RData', overwrite = T, dtoken = token)
+  
+  ## upload WOrkspace ####
+  drop_upload('save.RData', 'Master/R/', mode = "overwrite", dtoken = token)
+  
+  ## Script runterladen ####
+  #drop_download('Master/R/Master_ANN.R', overwrite = T, dtoken = token)
+  #file.edit('Master_ANN.R')
+  #drop_upload('Master_ANN.R', 'Master/R/', mode = "overwrite", dtoken = token)
+  
+  
