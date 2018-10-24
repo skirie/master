8b04d299bbf2e3d45e0f09144ba1b3d98e53d0dc
Ferdinand Briegel <ferdinand.briegel@meteo.local>
2018-10-24 15:29:52 +0200

    new compute full

--- functions.R
+++ functions.R
@@ -217,4 +217,115 @@ fun_model_compute <- function(df_train, params, units){
   return(list(all_rmse, all_r2))
 }
 
+#### Function model compute full ####
+fun_model_compute_full <- function(df_train, params, units){
+  ## params
+  k <- params[["k"]]
+  num_epochs <- params[["epochs"]]
+  optimizer <- params[["optimizer"]]
+  lr <- params[["lr"]]
+  layer <- params[["layer"]]
+  batch <- params[["batchsize"]]
+  
+  ## NA to 0
+  df_train[is.na(df_train)] <- 0
+  
+  ## empty vectors for Results
+  #all_rme_histories <- matrix(NA, nrow = k, ncol = num_epochs) 
+  all_rmse <- NULL
+  all_r2 <- NULL
+  
+  ## Callback - early stopping ####
+  callback_list <- list(callback_early_stopping(patience = 6))
+  
+  ## Model
+  model <- fun_build_model(df_train = df_train, layer = layer, optimizer = optimizer, units = units, lr = lr)
+  
+  ## Crossvalidation 2x k-fold crossvalidation
+  for (j in 1:2){
+    set.seed(j*5)
+    indices <- sample(1:nrow(df_train))
+    folds <- cut(indices, breaks = k, labels = FALSE)
+    
+    for (i in 1:k) {
+      cat("processing fold #", paste0(j, ".", i), "\n")
+      
+      # Prepare the train, validation and test data: data from partition # k
+      val_test_indices <- which(folds == i, arr.ind = TRUE)
+      
+      ## different datatype for different number of predictors 
+      if (ncol(df_train) == 2){
+        val_test_data <- as.array(df_train[, -ncol(df_train)][val_test_indices,])
+        partial_train_data <- as.array(df_train[, -ncol(df_train)][-val_test_indices,])
+        
+        # min and max for mormalization
+        mins_data <- min(partial_train_data, na.rm = T)
+        maxs_data <- max(partial_train_data, na.rm = T)
+      } else {
+        val_test_data <- as.matrix(df_train[, -ncol(df_train)][val_test_indices,])
+        partial_train_data <- as.matrix(df_train[, -ncol(df_train)][-val_test_indices,])
+        
+        # min and max for mormalization
+        mins_data <- apply(partial_train_data, 2, min, na.rm = T)
+        maxs_data <- apply(partial_train_data, 2, max, na.rm = T)
+      }
+      
+      ## same for target data
+      val_test_targets <- as.array(df_train[, ncol(df_train)][val_test_indices])
+      partial_train_targets <- as.array(df_train[, ncol(df_train)][-val_test_indices])
+      
+      # normalize all data
+      mins_targets <- min(partial_train_targets, na.rm = T)
+      maxs_targets <- max(partial_train_targets, na.rm = T)
+      
+      # split val_test into validation and test data
+      set.seed(i*5)
+      vt_indices <- sample(1:nrow(val_test_data))
+      vt_folds <- cut(vt_indices, breaks = 2, labels = FALSE)
+      
+      val_test_indices_2 <- which(vt_folds == 1, arr.ind = TRUE) 
+      val_data <- val_test_data[val_test_indices_2,]
+      val_targets <- val_test_targets[val_test_indices_2]
+      test_data <- val_test_data[-val_test_indices_2,]
+      test_targets <- val_test_targets[-val_test_indices_2]
+      
+      # predict = x
+      partial_train_data <- scale(partial_train_data, center = mins_data, 
+                                  scale = maxs_data - mins_data)
+      val_data <- scale(val_data, center = mins_data, 
+                        scale = maxs_data - mins_data)
+      test_data <- scale(test_data, center = mins_data, 
+                         scale = maxs_data - mins_data)
+      
+      # target = y
+      partial_train_targets <- scale(partial_train_targets, center = mins_targets, 
+                                     scale = maxs_targets - mins_targets)
+      val_targets <- scale(val_targets, center = mins_targets, 
+                           scale = maxs_targets - mins_targets)
+      test_targets <- scale(test_targets, center = mins_targets, 
+                            scale = maxs_targets - mins_targets)
+      
+      # fit model
+      history <- model %>% fit(
+        partial_train_data, partial_train_targets,
+        validation_data = list(val_data, val_targets),
+        epochs = num_epochs, batch_size = batch, verbose = 0,
+        callbacks = callback_list)
+      
+      # Evaluate the model on the validation data
+      #mae_history <- history$metrics$val_mean_absolute_error
+      #all_mae_histories[i,1:length(mae_history)] <- mae_history
+      
+      # predict and scale back
+      pred_test <- model %>% predict(test_data)
+      rmse_ <- rmse(test_targets, pred_test)
+      all_rmse <- rbind(all_rmse, rmse_)
+      r2 <- cor(test_targets, pred_test) ^ 2
+      all_r2 <- rbind(all_r2, r2)
+    }
+  }
+  return(list(all_rmse, all_r2))
+}
+#### ####
+
 #### ####
