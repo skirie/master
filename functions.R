ac163aecb40631c6ebfbeeb15f1fb4c19989909a
Ferdinand Briegel <ferdinand.briegel@meteo.local>
2018-12-12 11:15:57 +0100

    pre analysisi predictors

--- functions.R
+++ functions.R
@@ -220,6 +220,42 @@ fun_best_model <- function(df_results, params, type){
 #### Model Evaluation Functions ##
 #### ----------------------- ##
 
+## Pre analysisi Predictors ##
+fun_model_run_preanalysis_pred <- function(df_train, params){
+  ## params 
+  params[["layer"]] <- 2
+  params[["batchsize"]] <- 30
+  params[["units"]] <- 40
+  
+  ## required empty features / target 
+  all_mse <- vector("list", ncol(df_train)-1)
+  all_r2 <- vector("list", ncol(df_train)-1)
+  k <- ncol(df_train)-1
+  col_ <- colnames(df_train)
+  
+  ## loop for every predictor
+  for (i in 1:k){
+    # print computed predictor combination      
+    cat("Predictor: ", i, "/", ncol(df_train)-1, ". Used predictors:", col_[i], sep = "", "\n")
+    
+    # choose of one predictor and always Y. Y needs to be at last column position.
+    train_ <- df_train[, c(col_[i], col_[ncol(df_train)])]
+    
+    # compute Model for this predictor composition
+    cv_ <- fun_model_compute_full(df_train = train_, params = params, type = "pred")
+    mse_ <- mean(cv_[[1]], na.rm = T)
+    r2_ <- mean(cv_[[2]], na.rm = T)
+    all_mse[[i]] <- c(all_mse[[i]], mse_)
+    all_r2[[i]] <- c(all_r2[[i]], r2_)
+  }
+  
+  df_results <- data.frame(predictors = col_[1:k], mse = unlist(all_mse, use.names=FALSE), 
+                           r2 = unlist(all_r2, use.names=FALSE))
+  df_results <- df_results[order(df_results$mse),]
+  
+  return(df_results)
+}
+
 ## Function model run model structure ##
 fun_model_run_ms <- function(df_train, params){
   ## params 
