f3fc983431f4747fdad7f9111be0839e1a57b080
Ferdinand Briegel <ferdinand.briegel@meteo.local>
2018-12-12 15:31:16 +0100

    less

--- dropbox_link.R
+++ dropbox_link.R
@@ -6,18 +6,19 @@
   ## path 
   mypath <- getwd()
   
-#### Workspace speichern
-  save.image(paste0(mypath, "/RData/save.RData"))
-
 #### Verbindung zu dropbox aufbauen ####
   token <- readRDS(paste0(mypath,"/token.rds"))
   
+#### Workspace speichern
+  save.image(paste0(mypath, "/RData/save.RData"))
+  
   ## Workspace runterladen ####
   drop_download('Master/R/save.RData', paste0(mypath, "/RData/save.RData"), overwrite = T, dtoken = token)
-  
   load(paste0(mypath, "/RData/save.RData"))
   
   ## upload WOrkspace ####
-  drop_upload(paste0(mypath, "/RData/save.RData"), 'Master/R/', mode = "overwrite", dtoken = token)
+  drop_upload(paste0(mypath, "/RData/df_model.RData"), 'Master/R/', mode = "overwrite", dtoken = token)
   
-
+  
+  ls()
+  
