eaff014a20f74d14cb1304592d55633566feb19e
Ferdinand Briegel <ferdinand.briegel@meteo.local>
2018-12-14 16:08:22 +0100

    only one preselection

--- Excecution_file.R
+++ Excecution_file.R
@@ -44,18 +44,18 @@
 #### Model Selection for an moving window of 4 years ##
 #### ----------------------- ##
   
-  years_ <- unique(as.numeric(format(df_night_model$dt, "%Y")))
+  years_ <- unique(as.numeric(format(df_train.1$dt, "%Y")))
   results_pa <- vector("list", length(years_) - 3)
   results_ms <- vector("list", length(years_) - 3)
   
   for (i in 1:(length(years_) - 3)){
     window_ <- years_[i:(i + 3)]
-    df_train.2 <- df_night_model[which(as.numeric(format(df_night_model$dt, "%Y")) %in% window_), ]
+    df_train.2 <- df_train.1[which(as.numeric(format(df_train.1$dt, "%Y")) %in% window_), ]
     
-    results_pa[[i]] <- TargetPreAnalysisPredictors(df_train = df_train.2)
-    df_train.3 <- results_pa[[i]][[1]]
+    #results_pa[[i]] <- TargetPreAnalysisPredictors(df_train = df_train.2)
+    #df_train.3 <- results_pa[[i]][[1]]
     
-    results_ms[[i]] <- TargetFunBO(df_train = df_train.3, path = mypath) 
+    results_ms[[i]] <- TargetFunBO(df_train = df_train.2, path = mypath) 
   }
   
   
