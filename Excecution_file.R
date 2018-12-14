9236b3934f2673d029cfc59352f09876931cb3c8
Ferdinand Briegel <ferdinand.briegel@meteo.local>
2018-12-14 12:55:46 +0100

    bestpractice

--- Excecution_file.R
+++ Excecution_file.R
@@ -16,7 +16,7 @@
   
   ## Packages ####
   packages <- c("keras", "ggplot2", "Metrics", "httpuv", "rdrop2", "mlrMBO", "corrplot", "rgenoud", "betareg", "MASS")
-  check.packages(packages) 
+  CheckPackages(packages) 
   use_condaenv("r-tensorflow")
   #### ####
   
@@ -24,20 +24,20 @@
 #### Check for data preprocessing ##
 #### ----------------------- ##
   
-  fun_check_data()
+  CheckData()
 
 #### ----------------------- ##
 #### predictor pre analysis ##
 #### ----------------------- ##
   
-  pred_analysis <- fun_target_df_train(df_train = df_night_model)  
+  pred_analysis <- TargetPreAnalysisPredictors(df_train = df_night_model)  
   df_train_1 <- pred_analysis[[1]]
   
 #### ----------------------- ##
 #### Model Selection Respiration whole time span ##
 #### ----------------------- ##
   
-  results_resp_all <- fun_tagret_bo(df_train = df_train_1, path = mypath)
+  results_resp_all <- TargetFunBO(df_train = df_train_1, path = mypath)
   
   
   
