1535aa3ba6959f05f1ed9282b58dd5742a1a001f
irie irie <f.briegel@posteo.de>
2019-12-20 17:19:09 +0100

    tiff ENSO

--- 02_Plots_weather.R
+++ 02_Plots_weather.R
@@ -115,13 +115,13 @@
   
 #### Plots ####
   ## Monthly temperature and precipitation ####
-  pdf(paste0(path, "Data_and_Programming/Latex/Plots/Temp_prec_month.pdf"),
-      width = 16, height = 8, bg = "white")
+  tiff(paste0(path, "Latex/Plots/Temp_prec_month.tiff"),
+      width = 16, height = 8, bg = "white", units = 'in', res = 400)
 
   par(mfrow = c(3, 1))
 
   ## firgure for the diff of precipitation
-  par(mar = c(1, 7, 5 , 7.5))
+  par(mar = c(1, 8, 5 , 8))
   
   # plot
   barplot(df_weather_c_m$dif_p, axes = F, xaxt = 'n', beside = TRUE, 
@@ -133,10 +133,10 @@
   abline(h = 0)
   axis(3, at = seq(0.75, 13.9, length.out = 12), labels = month, cex.axis = cex_fig)
   axis(2, at = seq(-20, 40, 10), labels = c(-20, "", 0, "", 20, "", 40), las = 2, cex.axis = cex_axis)
-  mtext(text = expression(Delta*'P'*' [mm]'), side = 2, line = 4.4, cex = cex_legend)
+  mtext(text = expression(Delta*bolditalic('P')*' (mm)'), side = 2, line = 4.6, cex = cex_legend)
   
   ## figure for the 30year mean
-  par(mar = c(1, 7, 0, 7.5))
+  par(mar = c(1, 8, 0, 8))
   
   # precipitation barplot
   barplot(mean_p, space = 0, las = 1, xaxs = "i", ylab = NA, yaxt = "n", 
@@ -152,26 +152,27 @@
   abline(h = c(50, 100, 150, 200, 250), lty = 3, col = "grey30")
   axis(2, at = seq(0, 250, 50), labels = c(0, "", 100, "", 200, ""), las = 2,
        cex.axis = cex_axis)
-  mtext(text = "P [mm]", side = 2, line = 5, cex = cex_legend)
+  mtext(text = expression(paste(bolditalic("P")," (mm month"^"-1",")")), side = 2, line = 4.6, cex = cex_legend)
   
   # air temp
-  par(mar = c(0, 7, 0, 7.5), new = T)
+  par(mar = c(0, 8, 0, 8), new = T)
   plot(mean_t, type = "l", yaxt = "n", xaxt = "n", xlab = NA, ylab = NA,
        ylim = c(0, 20), lwd = 3, col = "firebrick", frame.plot = F)
   
   points(mean_t, cex = cex_fig, lwd = 3, col = "firebrick")
   lines(df_weather_c_m$mean_t, lty = 2, col = "red", lwd = 3)
+  points(df_weather_c_m$mean_t, cex = cex_fig, lwd = 3, col = "red")
   
-  legend(x = 0.8, y = 22, legend = c("15 year mean"), lty = c(2),
+  legend(x = 0.6, y = 22, legend = c("15 year mean"), lty = c(2), pch = 1,
          col = c("blue"), bg = F, bty = "n", cex = cex_axis, lwd = c(3))
-  legend(x = 3, y = 22, legend = c("30 year mean", "15 year mean"), lty = c(1, 2), 
+  legend(x = 3.1, y = 22, legend = c("30 year mean", "15 year mean"), lty = c(1, 2), pch = c(1, 1),
          col = c("firebrick", "red"), bg = F, bty = "n", cex = cex_axis, lwd = c(3, 3))
   
   axis(4, at = seq(0, 20, 5), labels = seq(0, 20, 5), las = 2,  cex.axis = cex_axis)
-  mtext(text = expression("T"[a]*" [°C]"), side = 4, line = 6, cex = cex_legend)
+  mtext(text = expression(bolditalic("T"[a])*" (°C)"), side = 4, line = 6.5, cex = cex_legend)
   
   ## firgure for the diff of temperature
-  par(mar = c(5, 7, 0, 7.5))
+  par(mar = c(5, 8, 0, 8))
   
   barplot(df_weather_c_m$dif_t, beside = TRUE, axes = F, xaxt = 'n', 
           las = 1, col = colors_m_t, yaxt ='n', cex.names = 1.3, 
@@ -184,37 +185,42 @@
   axis(1, at = seq(0.75, 13.9, length.out = 12), labels = month, 
        cex.axis = cex_fig, line = 0.7, lwd = 0)
   axis(4, at = c(-0.2, 0, 0.2, 0.4), las = 1,  cex.axis = cex_axis)
-  mtext(text = expression(Delta*'T'[a]*' [°C]'), side = 4, line = 6, cex = cex_legend)
+  mtext(text = expression(Delta*bolditalic('T'[a])*' (°C)'), side = 4, line = 6.5, cex = cex_legend)
   abline(h = 0)
   
   ## box
-  par(new = T, mfrow = c(1, 1), mgp = c(3, 1, 0), mar = c(3.3, 4.6, 3.3, 4.95))
+  par(new = T, mfrow = c(1, 1), mgp = c(3, 1, 0), mar = c(3.3, 5.3, 3.3, 5.3))
   plot(0, yaxt = "n", xaxt = "n", ylab = NA , xlab = NA, add = T, ylim = c(-2, -1))
   
   dev.off()
   
   ## Annual temperature and precipitation ####
-  pdf(paste0(path, "Data_and_Programming/Latex/Plots/Temp_prec_year.pdf"),
-      width = 16, height = 8, bg = "white")
+  # pdf(paste0(path, "Data_and_Programming/Latex/Plots/Temp_prec_year.pdf"),
+  #     width = 16, height = 8, bg = "white")
+  year_mean <- seq(0.75, 17.5, length.out = 15)
+  month <- (1.946429 - 0.75) / 12
+  
+  tiff(paste0(path, "Latex/Plots/Temp_prec_year.tiff"),
+       width = 16, height = 8, bg = "white", units = 'in', res = 400)
   
   par(mfrow = c(3, 1))
   
   ## figure for the diff of precipitation
-  par(mar = c(2, 8, 5, 7.5))
+  par(mar = c(2, 9, 5, 9))
   barplot(df_weather_c_y$dif_p, axes = F, xaxt = 'n', beside = TRUE, 
           las = 1, col = colors_y_p, yaxt = 'n', xaxs = "i",
-          ylim =c(-400, 520))
+          ylim =c(-400, 540))
   
   text(x = 0.5, y = 400, labels = "(a)", cex = cex_fig)
   abline(h = seq(-400, 500, 200), lty = 3, col = "grey30")
   abline(h = 0)
   axis(3, at = seq(0.75, 17.5, length.out = 15), labels = df_weather_c_y$year, cex.axis = cex_fig)
   axis(2, at = seq(-400, 500, 200), labels = seq(-400, 500, 200), las = 2, cex.axis = cex_axis)
-  mtext(text = expression(Delta*'P'*' [mm]'), side = 2, line = 5.4, cex = cex_legend)
+  mtext(text = expression(Delta*bolditalic('P')*' (mm)'), side = 2, line = 5.4, cex = cex_legend)
   
   ## figure for the 15 years
   # Precipitation
-  par(mar = c(1.5, 8, 0, 7.5))
+  par(mar = c(1.5, 9, 0, 9))
   plot(df_weather_c_y$prec, las = 1, lwd = 3, xaxt = "n", cex = cex_fig, axes = F,
           ylab = NA, yaxt = "n", col = "blue", ylim = c(0, 1700))
   lines(df_weather_c_y$prec, col = "blue", lwd = 3)
@@ -225,10 +231,10 @@
   text(x = 0.7, y = 1500, labels = "(b)", cex = cex_fig)
   axis(2, at = c(0, 500, 1000, 1500), labels = c(0, 500, 1000, 1500), las = 2,
        cex.axis = cex_axis)
-  mtext(text = "P [mm]", side = 2, line = 5.8, cex = cex_legend)
+  mtext(text = expression(paste(bolditalic("P")," (mm year"^"-1",")")), side = 2, line = 5.8, cex = cex_legend)
   
   # Temperature
-  par(mar = c(0, 8, 1, 7.5), new = T)
+  par(mar = c(0, 9, 1, 9), new = T)
   plot(df_weather_c_y$mean_t, type = "l", yaxt = "n", xaxt = "n", xlab = NA, ylab = NA,
        ylim = c(9.2, 11.5), lwd = 3, col = "firebrick", frame.plot = F)
   
@@ -237,13 +243,13 @@
   
   abline(h = 10, lty = 2, col = "red", lwd = 3)
   axis(4, at = seq(9.5, 11.5, 1), labels = seq(9.5, 11.5, 1), las = 2,  cex.axis = cex_axis)
-  mtext(text = expression("T"[a]*" [°C]"), side = 4, line = 6.5, cex = cex_legend)
+  mtext(text = expression(bolditalic("T"[a])*" (°C)"), side = 4, line = 8, cex = cex_legend)
   
   ## figure for the diff of temperature
-  par(mar = c(5, 8, 0, 7.5))
+  par(mar = c(5, 9, 0, 9))
   barplot(df_weather_c_y$dif_t, beside = TRUE, axes = F, xaxt = 'n', 
           las = 1, col = colors_y_t, yaxt ='n', cex.names = 1.3, 
-          xaxs = "i", ylab = NA, ylim = c(-0.7, 1.5))
+          xaxs = "i", ylab = NA, ylim = c(-0.9, 1.5))
   
   text(x = 0.5, y = 1.3, labels = "(c)", cex = cex_fig)
   abline(h = c(-0.5, 0, 0.5, 1, 1.5), lty = 3, col = "grey30")
@@ -253,16 +259,29 @@
        cex.axis = cex_fig, line = 0.7, lwd = 0)
   axis(4, at =  c(-0.5, 0, 0.5, 1, 1.5), las = 1, cex.axis = cex_axis)
   
-  legend(x = 6, y = 1.7, legend = c("annual P", "30 year mean"), lty = c(1, 2),
-         col = c("blue", "steelblue3"), bg = F, bty = "n", cex = cex_axis, lwd = c(3, 3))
-  legend(x = 9.5, y = 1.7, legend = c(expression("annual T"[a]), "30 year mean"), lty = c(1, 2), 
-         col = c("firebrick", "red"), bg = F, bty = "n", cex = cex_axis, lwd = c(3, 3))
+  rect(xleft = year_mean[1], ybottom = -0.85, xright = year_mean[2] - (4*month), 
+       ytop = -0.65, col = rgb(1, 0, 0, 0.3))
+  rect(xleft = year_mean[3] + month, ybottom = -0.85, xright = year_mean[4] - (4*month), 
+       ytop = -0.65, col = rgb(1, 0, 0, 0.3))
+  rect(xleft = year_mean[8] + month, ybottom = -0.85, xright = year_mean[9] - (3*month), 
+       ytop = -0.65, col = rgb(1, 0, 0, 0.3))
+  rect(xleft = year_mean[13] + (4*month), ybottom = -0.85, xright = year_mean[15] - month, 
+       ytop = -0.65, col = rgb(1, 0, 0, 0.3))
+  rect(xleft = year_mean[6] + month, ybottom = -0.85, xright = year_mean[7], 
+       ytop = -0.65, col = rgb(0, 0, 1, 0.3))
+  rect(xleft = year_mean[9], ybottom = -0.85, xright = year_mean[11] - (3*month), 
+       ytop = -0.65, col = rgb(0, 0, 1, 0.3))
   
-  mtext(text = expression(Delta*'T'[a]*' [°C]'), side = 4, line = 6.5, cex = cex_legend)
+  legend(x = 6, y = 1.7, legend = c(expression("annual "~italic("P")), "30 year mean"), lty = c(1, 2), pch = c(1, NA),
+         col = c("blue", "steelblue3"), bg = F, bty = "n", cex = cex_axis, lwd = c(3, 3))
+  legend(x = 9.5, y = 1.7, legend = c(expression("annual "~italic("T"[a])), "30 year mean"), lty = c(1, 2), 
+         col = c("firebrick", "red"), bg = F, bty = "n", cex = cex_axis, lwd = c(3, 3), pch = c(1, NA))
+  
+  mtext(text = expression(Delta*bolditalic('T'[a])*' (°C)'), side = 4, line = 8, cex = cex_legend)
   abline(h = 0)
   
   ## box 
-  par(new = T, mfrow = c(1, 1), mgp = c(3, 1, 0), mar = c(3.3, 5.3, 3.3, 4.95))
+  par(new = T, mfrow = c(1, 1), mgp = c(3, 1, 0), mar = c(3.3, 5.94, 3.3, 5.94))
   plot(0, yaxt = "n", xaxt = "n", ylab = NA , xlab = NA, add = T, ylim = c(-2, -1))
   
   dev.off()
